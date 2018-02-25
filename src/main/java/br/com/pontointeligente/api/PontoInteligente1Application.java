bbbe81cbd136612b011692dbeba6c29691d4be8f
Lucas Cezar <lmcezar@gmail.com>
2018-02-24 21:13:56 -0300

    Commit inicial dos arquivos do projeto

---
+++ src/main/java/br/com/pontointeligente/api/PontoInteligente1Application.java
@@ -0,0 +1,12 @@
+package br.com.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoInteligente1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligente1Application.class, args);
+	}
+}
